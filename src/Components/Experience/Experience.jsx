f11312c11c0009c5425da25150d97f83cde80fea
Aswankappingal <instamagic774@gmail.com>
2024-01-19 21:04:09 +0530

    ppp

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import './Experience.scss'
 
 const Experience = () => {
-  return (
-    <div className='fulll data'>
-    <div>
-        
-    <h4>My Abilities</h4>
+    
+    return (
+        <div className='fulll-data'>
+            <div className='Abilities'>
+
+                <h4>My Abilities</h4>
 
 
- </div>  
-      
-    </div>
-  )
+            </div>
+
+        </div>
+    )
 }
 
 export default Experience
