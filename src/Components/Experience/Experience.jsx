9dc84f094c281ace759738147c49643391c1de22
Aswankappingal <instamagic774@gmail.com>
2024-01-19 20:55:26 +0530

    pppp

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -3,7 +3,13 @@ import './Experience.scss'
 
 const Experience = () => {
   return (
+    <div className='fulll data'>
     <div>
+        
+    <h4>My Abilities</h4>
+
+
+ </div>  
       
     </div>
   )
