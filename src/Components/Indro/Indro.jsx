07def7e162eff3723840ff3b839d3eab7c141656
Aswankappingal <instamagic774@gmail.com>
2024-01-20 11:28:36 +0530

    ppp

--- src/Components/Indro/Indro.jsx
+++ src/Components/Indro/Indro.jsx
@@ -56,7 +56,7 @@ const Indro = () => {
                         </label>
                         <ul>
                             <li><a href="#" className="active" style={{ '--navAni': 1 }}>Home</a></li>
-                            <li><a href="##" style={{ '--navAni': 2 }}>About</a></li>
+                            <li><a href="#" style={{ '--navAni': 2 }}>About</a></li>
                             <li><a href="#" style={{ '--navAni': 3 }}>Skills</a></li>
                             <li><a href="#" style={{ '--navAni': 4 }}>Portfolio</a></li>
                             <li><a href="#" style={{ '--navAni': 5 }}>Contact</a></li>
