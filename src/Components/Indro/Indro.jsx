6f817a7e5439ba90921c1b354640de2992230878
Aswankappingal <instamagic774@gmail.com>
2024-02-12 16:43:56 +0530

    ppppp

--- src/Components/Indro/Indro.jsx
+++ src/Components/Indro/Indro.jsx
@@ -18,7 +18,7 @@ const Indro = () => {
           <input type="checkbox" id="click" />
           <label htmlFor="click" className="mainicon">
             <div className="menu" id="menu">
-              <i className="bi bi-listrrrr"></i>
+              <i className="bi bi-listrrr"></i>
             </div>
           </label>
           <nav>
