698d2e1bb084e6d7baf9f1cb41bd3fc786ebe64f
Aswankappingal <instamagic774@gmail.com>
2024-02-27 16:04:39 +0530

    pppp

--- src/Components/Indro/Indro.jsx
+++ src/Components/Indro/Indro.jsx
@@ -31,7 +31,7 @@ const Indro = () => {
         </header>
 
         <section>
-          <div className="mainnnnn">
+          <div className="mainnnn">
             <div className="Information">
               <h3>Hi, I'm Aswan</h3>
               <h1>Web Developer</h1>
